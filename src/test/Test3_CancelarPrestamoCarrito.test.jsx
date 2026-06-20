4f92646f07bc7a834457ee6d92e7c5ea6b696548
Nov-222 <is.gt.lar@gmail.com>
2026-06-19 22:59:57 -0400

    [HU-04] agregar test para funcionalidad de Cancelar Prestamo

---
+++ src/test/Test3_CancelarPrestamoCarrito.test.jsx
@@ -0,0 +1,48 @@
+import {test, beforeEach, expect, vi} from 'vitest'
+import { renderHook, act } from '@testing-library/react';
+import { useBiblioteca } from '../funcionalidades/biblioteca'
+
+
+beforeEach(() => {
+    vi.clearAllMocks();
+})
+
+vi.mock('../servicios/serviciolibro.js', () => ({
+    ObtenerLibros: vi.fn()
+}))
+
+import { ObtenerLibros } from '../servicios/serviciolibro';
+
+test('Funcionalidad de Cancelar Prestamo en Carrito', async() => {
+    const Libros = [
+        {
+            id: 1,
+            nombre: "La venganza de los Sith",
+            autor: "George Lucas",
+            portada: "portada.png",
+            copias : 3
+        }, {
+            id: 2,
+            nombre: "Tokyo Ghoul",
+            autor: "Sui Ishida",
+            portada: "portada2.png",
+            copias : 3
+        }
+    ]
+    ObtenerLibros.mockResolvedValue(Libros);
+    const {result} = renderHook(() => useBiblioteca())
+    await act(async() => {})
+
+
+    act(() => {
+        result.current.AgregarLibro(Libros[0]);
+    })
+
+    await act(async() => {
+        result.current.CancelarPrestamo();
+    })
+
+
+    expect(result.current.carrito.length).toBe(0);
+    expect(result.current.inventario[0].copias).toBe(3)
+})
